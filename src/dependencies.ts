07a049cc7cfe8a4ed34b9325f391984106b33aad
Alex Kit <source.kit@gmail.com>
2018-09-06 15:35:25 +0200

    v1.0.34

--- src/dependencies.ts
+++
@@ -1,7 +0,0 @@
-import * as utils from 'atma-utils'
-
-const globalIo = (<any>global).io;
-const io = globalIo && globalIo.File ? globalIo : require('atma-io');
-
-export { io, utils }
-
--- tools/build-dts.js
+++
@@ -1,9 +0,0 @@
-var dts = require('dts-bundle');
-
-dts.bundle({
-	name: 'atma-io-middleware-base',
-	main: './ts-temp/export.d.ts',
-	out: './out/index.d.ts'
-});
-
-io.File.copyTo('./ts-temp/out/index.d.ts', './lib/index.d.ts');
